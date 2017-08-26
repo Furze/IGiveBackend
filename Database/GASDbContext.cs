319357047e101f94dbd8da899eb94bad5b2641f6
LIAM <LIAM@DESKTOP-63VVUSA>
2017-08-26 22:10:02 +1200

    fixed build errors

--- Database/GASDbContext.cs
+++ Database/GASDbContext.cs
@@ -13,10 +13,6 @@ namespace GiveAShitBackend.Database
         public virtual DbSet<Product> Products { get; set; }
         public virtual DbSet<User> Users { get; set; }
         public virtual DbSet<ProductAssignment>ProductAssignments {get;set;}
-        public virtual DbSet<ProductCategory>ProductCategories { get; set; }
-        public virtual DbSet<TransactionProducts> TransactionProducts { get; set; }
-        public virtual DbSet<Donation> Donations { get; set; }
-        public virtual DbSet<UserTransaction> UserTransactions { get; set; }
         public virtual DbSet<Barcode> Barcodes { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
